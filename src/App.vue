d68ddfe0df0acabbdbf2e674c2e5b6bd0fbcf4c7
AWilson <dreamy26@icloud.com>
2021-07-12 10:23:45 -0400

    Changes the layout and inserts a card 4 fav dogs

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main class="dogs-layout">
+      <v-container fill-height>
+        <div class="dogs-overlay">
+          <h1 class="display-2 text-xs-center">Choose your favorite dogs</h1>
+          <v-card class="dog-card">
+            <v-img height="400px"></v-img>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>favorite</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>forward</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </div>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-img {
-  max-width: 100%;
-}
-
-h1 {
-  padding-bottom: 15px;
-}
-
-.dogs-layout {
-  width: 100%;
-  background: #fff center repeat;
-  background-image: url("https://github.com/FrontEndFoxes/projects/blob/main/petshop/images/bg3.jpg?raw=true");
-}
-
-.dogs-overlay {
-  width: 100%;
-  padding: 20px;
-  
-  background-color: #fff;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-@media (max-width: 768px) {
-  .dogs-overlay {
-    margin: 0;
-  }
-}
-
-.dog-card {
-  width: 100%;
-  max-width: 600px;
-}
-</style>
-
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from '@/plugins/vuetify';
 
 Vue.config.productionTip = false
 
+// This ensures that Vuetify's themes and components will be available throughout the Vue app.
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	vuetify,
+	render: (h) => h(App),
+}).$mount('#app');
+
